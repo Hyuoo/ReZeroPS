eee5c6d03fa0354cfd1487a53a5a9cf8f1a204d5
Hyuoo <43870227+Hyuoo@users.noreply.github.com>
2021-11-12 17:09:26 +0900

    G5 평범한 배낭

--- Baekjoon Online Judge/Gold/BOJ 12865.py
+++ Baekjoon Online Judge/Gold/BOJ 12865.py
@@ -1,12 +1,12 @@
 n, k = map(int,input().split())
-item =[]
+item = []
 for i in range(n):
     w, v = map(int,input().split())
     item += [[w,v]]
-dp=[[0]*(k+1)]
+dp = [[0]*(k+1)]
 for i in range(n):
     dp += [dp[i][:]]
     for j in range(k+1):
-        if(item[i][0]<=j and dp[i][j-item[i][0]]+item[i][1]>dp[i+1][j]):
+        if(item[i][0] <= j and dp[i][j-item[i][0]]+item[i][1] > dp[i+1][j]):
             dp[i+1][j] = dp[i][j-item[i][0]]+item[i][1]
 print(dp[n][k])
