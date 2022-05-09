b09e29630ac99fee2ead9a5355e9ff21bff37aaf
Hyuoo <43870227+Hyuoo@users.noreply.github.com>
2022-05-09 15:51:04 +0900

    메뉴 리뉴얼_2트

---
+++ Programmers/L2/p72411_a.py
@@ -0,0 +1,21 @@
+#LEVEL2
+#코딩테스트 연습 > 2021 KAKAO BLIND RECRUITMENT > 메뉴 리뉴얼
+#https://programmers.co.kr/learn/courses/30/lessons/72411
+
+#combinations란 놈을 알았다
+import itertools
+def solution(orders, course):
+    answer = []
+    for c in course:
+        comb = []
+        di = dict()
+        for o in orders:
+            comb += itertools.combinations(sorted(o),c)
+        for i in comb:
+            if(di.get(i)):
+                di[i] +=1
+            else:
+                di[i] = 1
+        if(di):
+            answer+=[key for key, val in di.items() if max(di.values())==val and max(di.values())>1]
+    return sorted(["".join(a) for a in answer])
