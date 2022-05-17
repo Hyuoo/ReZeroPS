304f787ec00eeccfb7378ee639efc56d7a666d17
Hyuoo <43870227+Hyuoo@users.noreply.github.com>
2022-05-17 14:49:41 +0900

    S3 미터계_숏

--- Baekjoon Online Judge/below silver/BOJ 1862_s.c
+++ Baekjoon Online Judge/below silver/BOJ 1862_s.c
@@ -1,2 +1,3 @@
 c;main(a,b){scanf("%d",&a);for(b=1;c+=(a%10-(a%10>4?1:0))*b,a/=10;b*=9);printf("%d",c);}
 //아니근데 파이썬이 더 짧넹 더 줄일수있나
+//o;main(a){for(;~(a=getchar());o+=a-48-(a>52))o*=9;printf("%d",o);} < 이거 왜ㅣ않ㄷ되
