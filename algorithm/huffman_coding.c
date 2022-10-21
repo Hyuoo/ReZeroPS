de2844d6739e391f4c41b435cfe830183158b95b
Hyuoo <43870227+Hyuoo@users.noreply.github.com>
2022-10-21 21:08:22 +0900

    Update huffman_coding.c

--- algorithm/huffman_coding.c
+++ algorithm/huffman_coding.c
@@ -20,22 +20,116 @@ void preOrder(node* Node) {
 		preOrder(Node->right);
 	}
 }
+void asdf(const char* sample);
 
 int main() {
 	char sample1[] = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
 	char sample2[] = "I am free today.";
 	char sample3[] = "aBcDeFgHIjK";
-	printf("기존 텍스트 길이 : %d\n", strlen(sample3));
+	
+	asdf(sample1);
+	asdf(sample2);
+	asdf(sample3);
+}
 
-	node* root = getFrequency(sample3);
+void asdf(const char* sample) {
+	node* root;
+	char* code;
+	char* originText;
+
+	printf("--------------------------\n");
+	printf("원래 문자열 : %s\n", sample);
+	printf("기존 텍스트 길이 : %d\n", strlen(sample));
+
+	root = getFrequency(sample);
+	code = huffmanEncoding(sample, root);
+	printf("CODE : %s\n", code);
+
+	originText = huffmanDecoding(code, root);
+	printf("ORIGIN : %s\n", originText);
+	printf("--------------------------\n");
+	free(root);
+	free(code);
+	free(originText);
+}
+
+void getCode(node* Node, char code, char depth, char table[27][2]) {
+	if (Node != NULL) {
+		if (Node->val != 0) {
+			if (Node->val == ' ') {
+				table[26][0] = code;
+				table[26][1] = depth;
+			}
+			else {
+				table[Node->val - 'A'][0] = code;
+				table[Node->val - 'A'][1] = depth;
+			}
+		}
+		else {
+			if (Node->left != NULL) {
+				getCode(Node->left, code, depth + 1, table);
+			}
+			if (Node->right != NULL) {
+				getCode(Node->right, code | (1 << depth), depth + 1, table);
+			}
+		}
+	}
 }
 
 char* huffmanEncoding(const char* str, node* map) {
+	//8개 비트, char형으로 경로 설정.
+	char table[27][2] = { 0, };
+	//[0]code [1]depth
+	getCode(map, 0, 0, table);
+	//코드 확인용
+	//for (int i = 0; i < 27; i++) {
+	//	printf("[%c : %d_%d]\n", i+'A', table[i][0],table[i][1]);
+	//}
+	char* encoded = malloc(sizeof(char)*2000);
+	int size = 0;
+	
 	char* ch;
+	char tmp;
 	ch = str;
+	//01문자열로 만들기
+	while (*ch != '\0') {
+		tmp = *ch==' '?26:((*ch) & (~('a' - 'A'))) - 'A';
+		for (int i = 0; i < table[tmp][1]; i++) {
+			encoded[size++] = table[tmp][0] & (1 << i) ? '1' : '0';
+		}
+		ch++;
+	}
+	encoded[size] = '\0';
+	return encoded;
 }
 
-char* huffmanDecoding(const char* str, node* map);
+char* huffmanDecoding(const char* str, node* map) {
+	node* tmp;
+	char* ch;
+	char* string = malloc(sizeof(char) * 100);
+	int size = 0;
+	
+	tmp = map;
+	ch = str;
+
+	while (*ch != '\0') {
+		if(*ch=='0'){
+			tmp = tmp->left;
+		}
+		else {
+			tmp = tmp->right;
+		}
+		if (tmp->val) {
+			//printf("%c", tmp->val);
+			string[size++] = tmp->val;
+			tmp = map;
+		}
+		ch++;
+	}
+	string[size] = '\0';
+
+	return string;
+}
 
 char* getFrequency(const char* str) {
 	node** table = malloc(sizeof(node*) * 27);
