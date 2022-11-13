69590aecddc86038b21b0677d7a3084d42dd9f8c
Hyuoo <43870227+Hyuoo@users.noreply.github.com>
2022-11-13 20:48:39 +0900

    1859. 백만 장자 프로젝트

--- SW Expert Academy/D2/swea 1859.py
+++ SW Expert Academy/D2/swea 1859.py
@@ -1,4 +1,7 @@
 T = int(input())
+#1. 전체 요소 중 최고값 sale
+#2. sale 인덱스 앞부분 전부 더해서(s), 그 갯수 n*sale 해서 이익 계산
+#3. sale 이후 배열 잘라서 요소가 1개이하 남을때까지 1.부터 반복
 for test_case in range(1, T + 1):
     _ = input()
     ar = list(map(int,input().split()))
