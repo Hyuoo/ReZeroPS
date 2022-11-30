2176f815f12f64f4e67bae285658bbaf6d64879a
Hyuoo <43870227+Hyuoo@users.noreply.github.com>
2022-12-01 00:04:24 +0900

    Update p138477.py

--- Programmers/L1/p138477.py
+++ Programmers/L1/p138477.py
@@ -1,6 +1,8 @@
 #https://school.programmers.co.kr/learn/courses/30/lessons/138477
 #코딩테스트 연습 > 연습문제 > 명예의 전당 (1)
 
+'''
+#그냥 풀이
 def solution(k, score):
     answer = []
     mj = []
@@ -12,4 +14,19 @@ def solution(k, score):
                 del mj[mj.index(min(mj))]
                 mj.append(sc)
         answer.append(min(mj))
+    return answer
+'''
+#힙을 이용한 풀이
+import heapq as h
+def solution(k, score):
+    answer = []
+    a=[]
+    for sc in score:
+        if len(a)>=k:
+            if a[0]<sc:
+                h.heappop(a)
+                h.heappush(a,sc)
+        else:
+            h.heappush(a,sc)
+        answer.append(a[0])
     return answer
