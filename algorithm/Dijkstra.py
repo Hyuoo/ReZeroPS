707d0e76490f8dc3af3ca56e8ff0e8540bad4fa0
Hyuoo <43870227+Hyuoo@users.noreply.github.com>
2022-12-26 16:30:32 +0900

    Update Dijkstra.py

--- algorithm/Dijkstra.py
+++ algorithm/Dijkstra.py
@@ -1,8 +1,32 @@
+#인접그래프에서 다익스트라 A->B 최단거리만을 리턴하는 함수
 
+INF = 9999999
 
 #그래프와 시작점, 도착점 입력받아 그 최단거리만을 리턴.
 def Dijkstra(maps:list, start:int, dest:int)->int:
-  pass
+  V = len(maps)
+  
+  visit = [False for _ in range(V)]
+  shortest = [INF for _ in range(V)]
+  
+  now = start
+  
+  while(now != dest):
+    visit[now] = True
+    
+    for i in range(V):
+      if maps[now][i]!=0 and maps[now][i]!=INF:
+        if shortest[i] < shortest[now] + maps[now][i]:
+          shortest[i] = shortest[now] + maps[now][i]
+    
+    min_idx = 0
+    for i in range(1,V):
+      if visit[i]==False and shortest[min_idx] > shortest[i]:
+        min_idx = i
+    
+    now = min_idx
+  
+  return shortest[dest]
 
 
 
