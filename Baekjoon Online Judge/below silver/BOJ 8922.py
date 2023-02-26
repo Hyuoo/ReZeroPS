4077f0a8278ee7b0ae8c8ff713f7a638d45bcec3
Hyuoo <43870227+Hyuoo@users.noreply.github.com>
2023-02-27 00:26:11 +0900

    S4 두찌 수열

--- Baekjoon Online Judge/below silver/BOJ 8922.py
+++ Baekjoon Online Judge/below silver/BOJ 8922.py
@@ -2,32 +2,34 @@ def dseq(n, a):
     tmp = [0 for _ in range(n)]
     for i in range(n):
         tmp[i] = abs(a[i]-a[i+1])
-    tmp.append(tmp[0])
-    return tmp
+    return tmp+[tmp[0]]
 
 for _ in range(int(input())):
     n=int(input())
-    mm=[[] for _ in range(15001)]
     a=list(map(int,input().split()))
     a.append(a[0])
     lp = 1
     for _ in range(1000):
         a = dseq(n,a)
-        s = sum(a)
-        if s==0:
+        if sum(a)==0:
             lp=0
             break
-        else:
-            aa = "".join(map(str, a))
-            for t in mm[s]:
-                if aa in t:
-                    # lp=1
-                    break
-            mm[s].append(aa*2)
     print("LOOP" if lp else "ZERO")
 
 '''
 두찌 수열
 풀이시간 : 29m
 
+else:
+    aa = "".join(map(str, a))
+    for t in mm[s]:
+        if aa in t:
+            # lp=1
+            break
+    mm[s].append(aa*2)
+괜히 이 코드로 조기퇴근 한다고 더 오래걸림
+
+이거 없앤 위 코드로
+33300KB -> 31256KB
+660ms -> 220ms
 '''
