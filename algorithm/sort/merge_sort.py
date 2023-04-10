10bded64e068e0b7b9af9f43bfb7483ff1b284ac
Hyuoo <43870227+Hyuoo@users.noreply.github.com>
2023-04-10 23:54:42 +0900

    Create merge_sort.py

---
+++ algorithm/sort/merge_sort.py
@@ -0,0 +1,26 @@
+#리스트 내 재정렬
+def merge_sort(arr:list, left:int, right:int)->None:
+    if right-left<2:
+        return
+    l = len(arr)
+    mid = (left+right)//2
+    merge_sort(arr, left, mid)
+    merge_sort(arr, mid, right)
+    new = []
+    i = left
+    j = mid
+    while i<mid and j<right:
+        if arr[i]<arr[j]:
+            new.append(arr[i])
+            i+=1
+        else:
+            new.append(arr[j])
+            j+=1
+    while i<mid:
+        new.append(arr[i])
+        i+=1
+    while j<right:
+        new.append(arr[j])
+        j+=1
+    for i in range(left,right):
+        arr[i] = new[i-left]
