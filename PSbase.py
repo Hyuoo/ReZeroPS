4262fd1a1193063f34a13cf509ad321f64dfe429
Hyuoo <43870227+Hyuoo@users.noreply.github.com>
2023-04-23 23:54:17 +0900

    Create PSbase.py

---
+++ PSbase.py
@@ -0,0 +1,18 @@
+import math
+from math import sqrt
+import heapq
+from collections import defaultdict
+from collections import Counter
+from collections import deque
+from functools import cmp_to_key
+from functools import reduce
+from itertools import permutations
+from itertools import combinations
+import random
+import sys
+#input = sys.stdin.readline
+#sys.setrecursionlimit(10**9)
+sys.stdin = open("input.txt", 'r')
+#MAX_LENGTH@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+'''CODE'''
