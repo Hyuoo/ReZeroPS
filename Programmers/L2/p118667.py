5fc9810709d34bc4886eb3cea16102e3ad2f5fa6
Hyuoo <43870227+Hyuoo@users.noreply.github.com>
2023-04-28 16:20:23 +0900

    2022 KAKAO TECH INTERNSHIP 두 큐 합 같게 만들기

--- Programmers/L2/p118667.py
+++ Programmers/L2/p118667.py
@@ -7,18 +7,13 @@ def solution(que1, que2):
     s = sum(que1)-sum(que2)
     tok = len(que1)*3
     count = 0
-    while tok:
-        if s:
-            if s>0:
-                s-=q1[0]*2
-                q2.append(q1.popleft())
-            else:
-                s+=q2[0]*2
-                q1.append(q2.popleft())
-            count+=1
+    while tok and s:
+        if s>0:
+            s-=q1[0]*2
+            q2.append(q1.popleft())
         else:
-            break
+            s+=q2[0]*2
+            q1.append(q2.popleft())
+        count+=1
         tok-=1
-    else:
-        count = -1
-    return count
+    return count if tok else -1
