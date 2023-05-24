df4fe844cb496088020c253d9a70213b8a831592
Hyungwoo Lim <43870227+Hyuoo@users.noreply.github.com>
2023-05-24 12:24:28 +0900

    다리를 지나는 트럭

--- Programmers/L2/p42583.py
+++ Programmers/L2/p42583.py
@@ -1,23 +1,41 @@
 #https://school.programmers.co.kr/learn/courses/30/lessons/42583
 
 def solution(bridge, w, tr):
-    time = [bridge] * len(tr)
-    i=j=count=0
+    time = [bridge]*len(tr)
+    i = j = count = 0
     while j<len(tr):
-        skip_time = 1
-        if i<j:
-            if tr[j]>w:
-                skip_time = time[i]
-            for k in range(i,j):
-                time[k] -= skip_time
-        if time[i]==0:
+        skip = time[i] if tr[j]>w else 1
+        for mov in range(i,j):
+            time[mov] -= skip
+        while time[i]<=0:
             w+=tr[i]
             i+=1
         if tr[j]<=w:
             w-=tr[j]
             j+=1
-        count += skip_time
+        count+=skip
     return count+bridge
+
+# 구 풀이
+# def solution(bridge, w, tr):
+#     time = [bridge] * len(tr)
+#     i=j=count=0
+#     while j<len(tr):
+#         skip_time = 1
+#         if i<j:
+#             if tr[j]>w:
+#                 skip_time = time[i]
+#             for k in range(i,j):
+#                 time[k] -= skip_time
+#         if time[i]==0:
+#             w+=tr[i]
+#             i+=1
+#         if tr[j]<=w:
+#             w-=tr[j]
+#             j+=1
+#         count += skip_time
+#     return count+bridge
+
 '''
 다리를 지나는 트럭
 풀이시간 : 30분쯤?
@@ -41,4 +59,13 @@ j가 더 없으므로
 경과시간 + 다리길이가 정답
 ** 다리에있는 가장 마지막 차(=마지막에 들어가는 차)가 나가는 시간인데
   어차피 다리 길이만큼 진행하므로 다리길이 더하면 정답
+====================================
+최신풀이에서
+나오는 차를 while로 다 빼니까 무조건 다리에는 차가 있음.
+    ==> if i<j가 불필요해짐
+    
+    ..어 근데 이제보니까 예전 코드에 오류있는데?
+        - 이 코드도 무조건 i<j이긴 해서 발생안하는 오류긴한데,,
+        - i<j일때만 skip_time이 되는데,
+        - skip_time이 없는 경우에도 count는 skip_time만큼 더해줌.
 '''
