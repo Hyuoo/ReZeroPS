af670d1f8ad3651fc850432b3d6e4cc7a91b7c50
Hyungwoo Lim <43870227+Hyuoo@users.noreply.github.com>
2023-07-13 02:42:29 +0900

    Update quick_sort.py

--- algorithm/sort/quick_sort.py
+++ algorithm/sort/quick_sort.py
@@ -1,28 +1,28 @@
-def swap(arr, a,b):
-    #print("SWAP to ",a,"&",b)
+def swap(arr, a, b):
+    #print("SWAP to ", a, "&", b)
     tmp = arr[a]
     arr[a] = arr[b]
     arr[b] = tmp
 
-def qsort(arr, s, e):
-    if(s>=e):
+def qsort(arr, start, end):
+    if(start >= end):
         return
-    key = s
-    l = s+1
-    r = e
-    while(l<r):
-        while(l<r and arr[l]<=arr[key]):
-            l+=1
-        while(l<=r and arr[r]>arr[key]):
-            r-=1
-        if(l<r):
-            swap(arr,l,r)
-    #if(key!=r and arr[key]>arr[r]):
-    swap(arr,key,r)
-    qsort(arr,s, r-1)
-    qsort(arr,r+1,e)
+    key = start
+    left = start+1
+    right = end
+    while(left<right):
+        while(left<right and arr[left]<=arr[key]):
+            left+=1
+        while(left<=right and arr[right]>arr[key]):
+            right-=1
+        if(left<right):
+            swap(arr, left, right)
+    #if(key!=right and arr[key]>arr[r]):
+    swap(arr, key, r)
+    qsort(arr, start, r-1)
+    qsort(arr, r+1, end)
 
 if __name__ == "__main__":
     import random
     arr = [int(random.random()*100) for _ in range(10000)]
-    qsort(arr,0,len(arr)-1)
+    qsort(arr, 0, len(arr)-1)
