403942ea0074628a4c00d8f26e1f7e2a61d654f7
Hyuoo <hopeace6@naver.com>
2023-11-04 18:58:22 +0900

    Update kmp.py

--- algorithm/string/kmp.py
+++ algorithm/string/kmp.py
@@ -1,4 +1,23 @@
+"""
+# KMP
+- (Knuth, Morris, Prett) 세 명이서 만든 알고리즘.
 
+문자열에서 패턴이 일치하는 인덱스를 **모두** 찾는 알고리즘.
+근데, 매우 빠르게 O(N+M) 시간에 문자열 검색을 할 수 있다.
+(N: 텍스트의 길이, M: 패턴의 길이)
+단순/무식하게 완전탐색으로 검색하는 방법은 O(NM)
+
+접두사(prefix) 접미사(suffix)를 활용하여 풀이.
+
+=====
+### pi배열
+pi[i] == i인덱스까지
+prefix == suffix가 될 수 있는 부분 문자열 중
+가장 긴 것의 길이.
+(단, prefix != i까지 부분문자열)
+
+[출처: https://bowbowbow.tistory.com/6]
+"""
 # pi.
 # string p의 길이 i인 서브스트링에서 가장 긴 prefix==suffix 길이 리스트로 반환.
 def getpi(p:str)->list:
@@ -32,6 +51,33 @@ def kmp(s:str, p:str)->list:
                 j+=1
     return ans
 
-k=kmp(input(),input())
-print(len(k))
-print(*k)
+if __name__ == "__main__":
+    text = "ABCABABCDEFAAABC"
+    pattern = "ABC"
+    p_l = len(pattern)
+    k=kmp(text, pattern)
+
+    tmp = [" "]*len(text)
+    for i in k:
+        tmp[i] = "*"
+    print(text)
+    print("".join(tmp))
+
+    print("search count:", len(k))
+    print("search indexes:", k)
+
+    if "getpi examples" and 0:
+        s = ["ABCDABCDABEED",
+             "ABCABCACC",
+             "ABCDCBA",
+             "ABCDABCD",
+             "ABABABABA",
+             "ABABABABAB",
+             "ABABCABCABD",
+             "ABCAABCABCABC"]
+        print("\ngetpi examples:")
+        for i in s:
+            print(*i)
+            print(*getpi(i))
+            print(kmp(i, "ABC"))
+            print("-"*40)
