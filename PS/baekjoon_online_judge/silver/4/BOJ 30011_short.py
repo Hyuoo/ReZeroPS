6d1584ecab1be4c97e74aa7bdcd939ecd9f3eb45
Hyuoo <hopeace6@naver.com>
2023-11-29 23:06:31 +0900

    short coding

---
+++ PS/baekjoon_online_judge/silver/4/BOJ 30011_short.py
@@ -0,0 +1,11 @@
+"""
+Solving Date    : 2023.11.27
+Title           : 겹다각형의 각
+tags            : 수학, 기하학
+url             : https://www.acmicpc.net/problem/30011
+rank            : 4
+byte            : 50 B
+"""
+
+_,a=open(0)
+print((sum(map(int,a.split()))-2)*180)
