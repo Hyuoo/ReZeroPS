0dc99f794b8c4538266a0129c2023c7a4a3ee333
Hyuoo <hopeace6@naver.com>
2024-02-04 18:21:55 +0900

    solve: string, implement

--- PS/programmers/L2/p17686.py
+++ PS/programmers/L2/p17686.py
@@ -8,38 +8,20 @@ runtime         : -
 memory          : -
 """
 
-from functools import cmp_to_key
-
 def split_file_name(file_name):
     tmp = ""
     flag = 0
     for ch in file_name:
-        if flag == 0 and "0" <= ch <= "9":
+        if flag == 0 and "0"<=ch<="9":
             yield tmp.lower()
             tmp = ""
             flag += 1
-        elif flag == 1 and not "0" <= ch <= "9":
+        elif flag == 1 and not "0"<=ch<="9":
             yield int(tmp)
-            tmp = ""
             flag += 1
         tmp += ch
     if flag == 1:
         yield int(tmp)
-    else:
-        yield tmp
-
-
-def cmp_func(a, b):
-    if a[1] > b[1]:
-        return 1
-    elif a[1] < b[1]:
-        return -1
-    else:
-        if a[2] > b[2]:
-            return 1
-        elif a[2] < b[2]:
-            return -1
-    return 0
 
 
 def solution(files):
@@ -50,17 +32,14 @@ def solution(files):
     # for file in files:
     #     print(file, ":", list(split_file_name(file)))
 
-    return [files[i[0]] for i in sorted([[idx] + list(split_file_name(file)) for idx, file in enumerate(files)], key=cmp_to_key(cmp_func))]
+    return [i[0] for i in sorted([[file]+list(split_file_name(file)) for file in files], key=lambda x:(x[1], x[2]))]
 
 """
 2018 KAKAO BLIND RECRUITMENT [3차] 파일명 정렬
 
 - head, number, tail 세 부분 잘라서
 - 원래 인덱스를 포함하여 새 리스트를 만들고
-- 정렬 기준대로 정렬해서
-- 원래 인덱스 위치의 값으로 리스트를 만들었다.
-
-cmp_to_key를 사용해서 커스텀 정렬기준으로 정렬했다.
+- 정렬 기준대로 정렬
 
 
 .. SQL아니고 코드에서는 정규식이랑 안친해진다.
@@ -70,8 +49,8 @@ cmp_to_key를 사용해서 커스텀 정렬기준으로 정렬했다.
 막 하다보니 숏코딩이 되어 대강 풀어 쓴 버전
 
 tmp = []
-for idx, file in enumerate(files):
-    tmp.append([idx]+list(split_file_name))
-sorted_ar = sorted(tmp, key=cmp_to_key(cmp_func))
-return [files[i[0]] for i in sorted_ar]
+for file in files:
+    tmp.append([file]+list(split_file_name(file)))
+sorted_ar = sorted(tmp, key=lambda x:(x[1], x[2]))
+return [i[0] for i in sorted_ar]
 """
