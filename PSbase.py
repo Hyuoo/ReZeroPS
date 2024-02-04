1de1dba283ec29ff585ae71084382ebf460bbb46
Hyuoo <hopeace6@naver.com>
2024-02-04 18:11:23 +0900

    Update PSbase.py

--- PSbase.py
+++ PSbase.py
@@ -1,6 +1,9 @@
+import itertools
 import math
+import timeit
 from math import sqrt
 import heapq
+import bisect
 from collections import defaultdict
 from collections import Counter
 from collections import deque
@@ -8,24 +11,17 @@ from functools import cmp_to_key
 from functools import reduce
 from itertools import permutations
 from itertools import combinations
+from itertools import combinations_with_replacement
 import random
+import copy
 from typing import *
 import sys
 # input = sys.stdin.readline
+# input = lambda:sys.stdin.readline.rstrip()
 # sys.setrecursionlimit(10**9)
 sys.stdin = open("input.txt", 'r')
 from tools import timecheck
+import dis
 #MAX_LENGTH_79@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-
 '''CODE'''
-
-"""
-Solving Date    : 
-Solving Time    : 
-Title           : 
-tags            : 
-url             : 
-runtime         : 
-memory          : 
-"""
 
